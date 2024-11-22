7caa2e9eda2b91e0a2dff2bcb4204e6799c0cb44
ddwivedi2003 <135425359+ddwivedi2003@users.noreply.github.com>
2024-11-22 15:21:56 +0000

    fix

--- setup.sh
+++ setup.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-
-# Update package lists
 sudo apt-get update
-
-# Install distutils
 sudo apt-get install -y python3-distutils
-
-# Install Tesseract OCR
 sudo apt-get install -y tesseract-ocr
-
-# Install Python dependencies
 pip install -r requirements.txt
